07d7f63921fa4daf76f103e88c2de1065a9486cf
albright.human <sungutialbright@gmail.com>
2022-05-16 20:32:19 +0300

    ft:initialize app and database

---
+++ manage.py
@@ -0,0 +1,27 @@
+from app import create_app
+from app import db
+from app.models import User
+
+# from flask_migrate import Migrate #MigrateCommand
+
+from flask.cli import FlaskGroup
+
+
+# Creating app instance
+app = create_app('development')
+cli = FlaskGroup(app)
+
+
+@cli.command('test')
+def test():
+    """Run the unit tests."""
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
+@cli.command('shell')
+def make_shell_context():
+    return dict(app = app,db = db,User = User )
+
+if __name__ == '_main_':
+    cli()
---
+++ start.sh
@@ -0,0 +1,19 @@
+export SECRET_KEY= 'secret'
+export MAIL_USERNAME='sungutialbright@gmail.com'
+export MAIL_PASSWORD='travisasutsa01'
+
+export FLASK_APP='app:create_app("development")'
+# change the command from server to run
+python manage.py run
+
+
+#how to run the app:
+#  env FLASK_APP='app:create_app("development")' flask run
+#how to initialize db:
+#  env FLASK_APP='app:create_app("development")' flask db init
+
+#how to migrate:
+#  env FLASK_APP='app:create_app("development")' flask db migrate
+
+#how to upgrade db:
+#  env FLASK_APP='app:create_app("development")' flask db upgrade
